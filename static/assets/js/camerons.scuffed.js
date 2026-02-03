3779a38626779d09c5cf6cce530b37833ca3c503
camjayreed <xtswolf2003@gmail.com>
2026-02-03 06:07:40 -0600

    One small step for monkey, one giant step for monkey kind

---
+++ static/assets/js/camerons.scuffed.js
@@ -0,0 +1,7 @@
+document.getElementById("li-witness").addEventListener("click", witness);
+
+function witness() {
+    console.log("witness monke")
+
+    document.getElementById("bg").classList.toggle("active");
+}
