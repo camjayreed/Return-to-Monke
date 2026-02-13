92dc23b1127874058cb409766103d48fac6ba842
camjayreed <xtswolf2003@gmail.com>
2026-02-13 07:46:20 -0600

    implemented 2 chat rooms, both currently with the ability to write text to the console. working on getting it displayed live, then ill work on making it cross users with websockets

--- static/assets/js/camerons.scuffed.js
+++ static/assets/js/camerons.scuffed.js
@@ -1,5 +1,7 @@
 let ws; // global ws to be set on load, so its accessible everywhere
 
+//// INITIAL WEBSOCKET SETUP ////
+
 // create our websocket connection on page loading
 document.addEventListener("DOMContentLoaded", () => {
   ws = new WebSocket("ws://localhost:8765");
@@ -30,4 +32,59 @@ document.getElementById("li-witness").addEventListener("click", witness);
 function witness() {
   // send message to backend telling it our image changed
   ws.send("IMAGE_STATE");
+}
+
+//// MONKEY CHAT ////
+
+// just a function that takes the id of an input field
+// and allows you to press enter from within the input field to submit instead of having to tie a submit button to it
+function submit_on_enter(input_id) {
+  var input = document.getElementById(`${input_id}`);
+
+  // Execute a function when the user presses a key on the keyboard
+  input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+
+      const input_text = input.value
+      test_func(input_text)
+    }
+  });
+}
+
+// send the users inputted text to our chatroom
+function test_func(text) {
+  console.log("submit_chat:", text)
+}
+
+// what to do upon opening chat 1
+document.getElementById("chat1_btn").addEventListener("click", chat1)
+function chat1() {
+  // setup divs
+    let chat1_box = document.getElementById("chat1_div")
+    let chat1_usertxt = document.createElement("p")
+    // hide chat 2
+    document.getElementById("chat2_div").setAttribute("hidden", "")
+    // show chat 1
+    document.getElementById("chat1_div").removeAttribute("hidden")
+
+  // setup chat input
+    submit_on_enter("chat1_txt")
+}
+
+// what to do upon opening chat 2
+document.getElementById("chat2_btn").addEventListener("click", chat2)
+function chat2() {
+
+  // setup divs
+    // hide chat 1
+    document.getElementById("chat1_div").setAttribute("hidden", "")
+    // show chat 2
+    document.getElementById("chat2_div").removeAttribute("hidden")
+
+    // setup chat input
+      submit_on_enter("chat2_txt")
 }
